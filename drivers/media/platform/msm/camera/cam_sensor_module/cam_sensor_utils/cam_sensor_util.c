6b92b10ae134b2515b329b76d43ba3980b02806d
Demon000 <demonsingur@gmail.com>
2019-05-14 10:05:53 +0200

    Revert "msm: camera: adjust unconditional wait rules"
    This reverts commit 3400e0e32b2b88a403d11147aa9038d1000b0ca8.

--- drivers/media/platform/msm/camera/cam_sensor_module/cam_sensor_utils/cam_sensor_util.c
+++ drivers/media/platform/msm/camera/cam_sensor_module/cam_sensor_utils/cam_sensor_util.c
@@ -92,13 +92,11 @@ int32_t cam_sensor_handle_delay(
 		i2c_list =
 			list_entry(list_ptr, struct i2c_settings_list, list);
 		if (generic_op_code ==
-			CAMERA_SENSOR_WAIT_OP_HW_UCND && cmd_uncond_wait->delay < 1000)
+			CAMERA_SENSOR_WAIT_OP_HW_UCND)
 			i2c_list->i2c_settings.reg_setting[offset - 1].delay =
 				cmd_uncond_wait->delay;
 		else
-			i2c_list->i2c_settings.delay =
-				(cmd_uncond_wait->delay + 500) / 1000;
-
+			i2c_list->i2c_settings.delay = cmd_uncond_wait->delay;
 		(*cmd_buf) +=
 			sizeof(
 			struct cam_cmd_unconditional_wait) / sizeof(uint32_t);
